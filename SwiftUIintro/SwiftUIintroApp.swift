a86a6940b89395fef222c96ba0b724ebed36665a
jperez1478 <61983873+jperez1478@users.noreply.github.com>
2021-08-01 12:08:47 -0500

    Initial Commit

---
+++ SwiftUIintro/SwiftUIintroApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIintroApp.swift
+//  SwiftUIintro
+//
+//  Created by Jessica Perez on 8/1/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIintroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
