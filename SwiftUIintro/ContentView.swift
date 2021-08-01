5556164dea2017a78929527c9f3bb8de902a252a
jperez1478 <61983873+jperez1478@users.noreply.github.com>
2021-08-01 12:20:42 -0500

    1. Added a stack 2. inside that stack i added a modifier text with large tittle 3. Created a button that will print out a string in terminal

--- SwiftUIintro/ContentView.swift
+++ SwiftUIintro/ContentView.swift
@@ -9,8 +9,17 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack  {
+            //modifiers text display name
+            Text("Hello, Jessica")
+                .font(.largeTitle)
+            //CREATE a button
+            Button(action: {
+                print("hello jess")
+            }) {
+                Text("Press Me")
+            }
+        }
     }
 }
 
